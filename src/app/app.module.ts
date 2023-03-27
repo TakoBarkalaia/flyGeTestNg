97f747397c784710e269a6b48d26a3d173561443
Tako Barkalaia <114744524+TakoBarkalaia@users.noreply.github.com>
2023-03-27 20:42:46 +0400

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftSideComponent } from './left-side/left-side.component';
+import { RightSideComponent } from './right-side/right-side.component';
+import { MobileComponent } from './mobile/mobile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    MobileComponent
   ],
   imports: [
     BrowserModule,
